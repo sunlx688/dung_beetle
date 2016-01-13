4eaeaedf62e7477710bd424a4b6548cc3fb119ac
sunlx688 <sunlx688@163.com>
2016-01-13 21:59:05 +0800

    转换为Python3

--- get_doubant.py
+++ get_doubant.py
@@ -1,33 +1,36 @@
-#使用BeautifulSoup获取豆瓣新片榜， 口碑榜， 票房榜
+# 使用BeautifulSoup获取豆瓣新片榜， 口碑榜， 票房榜
 
-import re, urllib
+import re
 from bs4 import BeautifulSoup
+import requests
 
 raw_url = 'http://movie.douban.com/chart'
-html = urllib.urlopen(raw_url).read()
-
+html = requests.get(raw_url).text
 
 reg_name = r'<img src=".*" alt="(.*?)" class=""/>'
 reg_introduction = r'<p class="pl">(.*)</p>'
-reg_score= r'<span class="rating_nums">(.*?)</span>'
+reg_score = r'<span class="rating_nums">(.*?)</span>'
 reg_evalute = r'<span class="pl">\((.*)\)</span>'
 
-print '豆瓣新片排行榜'
-print '-' * 80
+print('豆瓣新片排行榜')
+print('-' * 80)
+
 
 class DBM(object):
     def __init__(self, reg, html):
         self.reg, self.html = reg, html
+
     def __iter__(self):
-        return self  #instance self is iteration object, so return self
-    
+        return self  # instance self is iteration object, so return self
+
     def get(self):
         m_list = re.findall(self.reg, self.html)
-        u_list =[]
+        u_list = []
         for abc in m_list:
-            u_abc = abc.decode('utf-8')
+            u_abc = abc
             u_list.append(u_abc)
         return u_list
+
 
 name = DBM(reg_name, html)
 a = name.get
@@ -43,8 +46,9 @@ d = evaluate.get
 
 
 def index(num):
-    print a()[num], ' ',  b()[num], ' ', 'Score:%s'%(c()[num]),' ', d()[num]
-    print ''
+    print(a()[num], ' ', b()[num], ' ', 'Score:%s' % (c()[num]), ' ', d()[num])
+    print('')
+
 
 nn = len(a())
 for x in range(nn):
@@ -52,40 +56,38 @@ for x in range(nn):
 
 
 # use bs4
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, 'html.parser')
 
-#本周口碑榜
+# 本周口碑榜
 week = soup.find('div', id='ranking').find('ul', id='listCont2')
-print '-' * 80
-print '本周口碑榜....', soup.find('div', id='ranking').find('ul', id='listCont2') \
-      .find('li').get_text()
+print('-' * 80)
+print('本周口碑榜....', soup.find('div', id='ranking').find('ul', id='listCont2') \
+      .find('li').get_text())
 for link in week.find_all('a'):
-    print link.get_text()
+    print(link.get_text())
 
 
-#北美票房榜影名
+# 北美票房榜影名
 week_name = soup.find('div', id='ranking').find('ul', id='listCont1')
 america = []
 for link in week_name.find_all('a'):
     america.append(link.get_text())
 
-#票房榜的钱
+# 票房榜的钱
 money = soup.find('div', id='ranking').find('ul', id='listCont1')
 dollar = []
 for m in money.find_all('span'):
     dollar.append(m.get_text())
 m_date = dollar.pop(0)
 
+
 def split(num2):
-    print america[num2], dollar[num2]
+    print(america[num2], dollar[num2])
 
 
-print '-' * 80
-print '北美票房榜....', m_date
+print('-' * 80)
+print('北美票房榜....', m_date)
 
 lens = len(america)
 for num2 in range(lens):
     split(num2)
-
-
-
